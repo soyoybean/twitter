38aab5d1f1361500e15a37b54500b45ad4052b3d
soyoybean <sok020@ucsd.edu>
2020-02-12 21:50:28 -0800

    pull to refresh

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -13,12 +13,16 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweet()
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
     
-    func loadTweet(){
+    @objc func loadTweet(){
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count" : 10]
         TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success: { (tweets: [NSDictionary]) in
@@ -28,6 +32,7 @@ class HomeTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             print("Could not retreive tweets! oh no!")
