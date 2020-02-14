8946fee1937e3324bcb4a887db894c220ae31f49
soyoybean <sok020@ucsd.edu>
2020-02-13 18:11:46 -0800

    user can tweet texts

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,9 +17,14 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
+        //loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
     }
     
     @objc func loadTweets(){
